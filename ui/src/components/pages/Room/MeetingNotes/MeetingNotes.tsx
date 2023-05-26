f542209b342c3a1e28e396d940a38ab58bade9af
mariz <marrise@oostvoort.com>
2023-05-26 16:28:10 +0800

    fix: import react-quill issue

--- ui/src/components/pages/Room/MeetingNotes/MeetingNotes.tsx
+++ ui/src/components/pages/Room/MeetingNotes/MeetingNotes.tsx
@@ -8,7 +8,7 @@ import {WebsocketProvider} from "y-websocket";
 import {useParams} from "react-router-dom";
 import useCollaborate from "../../../../hooks/useRoom/useCollaborate";
 
-import * as ReactQuill from "react-quill";
+import ReactQuill from "react-quill";
 
 function MeetingNotesComponents() {
   const quillRef = useRef<ReactQuill>(null);
